50fa87569d384c1511194466c82222aeabc0f933
Brijesh Chandra <23568670+Brijesh-Chandra@users.noreply.github.com>
2018-11-29 17:20:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ var fs = require("fs")
 
 http.createServer(function(req, res){
 	
-	if(req.url === "/"){
-		res.writeHead(200, {"Content-Type": "text/html"});
-		fs.createReadStream("./sudoku.htm", "UTF-8").pipe(res);	
-	} 
-	else if (req.url === "/styles.css"){
+	if (req.url.match("\.css$")){
 		res.writeHead(200, {"Content-Type": "text/css"});
-		fs.createReadStream("./styles.css", "UTF-8").pipe(res);
+		fs.createReadStream("styles.css", "UTF-8").pipe(res);
 	}
-	else if (req.url === "/sudoku.js"){
+	else if (req.url.match("\.js$")){
 		res.writeHead(200, {"Content-Type": "text/javascript"});
-		fs.createReadStream("./sudoku.js", "UTF-8").pipe(res);
+		fs.createReadStream("sudoku.js", "UTF-8").pipe(res);
 	}
-					
-}).listen(8080);
+	else {
+		res.writeHead(404, {"Context-Type": "text/html"});
+		fs.createReadStream("sudoku.htm", "UTF-8").pipe(res);	
+	}
+				
+}).listen(3000);
 
-console.log("Sudoku game is on port 8080");
+console.log("Sudoku game is on port 3000");
